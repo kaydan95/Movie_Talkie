ddf7a838719322a5f192934fdfb4844578836aaa
kaydan95 <kyori0515@gmail.com>
2022-05-13 00:37:48 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -67,7 +67,8 @@ const main = async () => {
         schema,
         graphiql : true,
     }));
-    app.use(express.static('client/build'));
+    app.use(express.static('client'));
+    app.use(express.static('build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve('./client/build', './index.html'));
     });
