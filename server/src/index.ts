a0a416f2b10eebef8aa80672e53e4a9f55722a95
kaydan95 <kyori0515@gmail.com>
2022-05-12 18:25:43 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -67,7 +67,7 @@ const main = async () => {
         graphiql : true,
     }));
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
 
