7915bbf84894b159ba225839e671d4da6c276fb3
kaydan95 <kyori0515@gmail.com>
2022-05-12 16:27:54 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -66,9 +66,9 @@ const main = async () => {
         schema,
         graphiql : true,
     }));
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
 
     const PORT = process.env.PORT || 3001
