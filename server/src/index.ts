2fdcb7368ae8867be9b0d92e2a63960d9b7bae74
kaydan95 <kyori0515@gmail.com>
2022-05-13 01:39:53 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -75,7 +75,7 @@ const main = async () => {
 
     const port = process.env.PORT || 3001
 
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log(process.env.PORT);
         console.log(`🚀 Server ready`);
     })
