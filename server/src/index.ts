1048c3a7ba8459886b8b0276372a868f24a50f4c
kaydan95 <kyori0515@gmail.com>
2022-05-15 02:00:18 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -66,20 +66,24 @@ const main = async () => {
         schema,
         graphiql : true,
     }));
-    if(process.env.NODE_ENV == "production"){
-        // app.use(path.join(__dirname, "./../../client/build"));
-        // app.get('*', (req, res) => {
-        //     res.sendFile(path.join(__dirname, "./../../client/build", "index.html"));
-        // });
-        app.listen(process.env.PORT, () => {
-            console.log(`🚀 Server ready`);
-        })
-    }
-    else {
-        app.listen(3001, () => {
-            console.log(`🚀 Server ready at http://localhost:3001/graphql`);
-        })
-    }
+    // if(process.env.NODE_ENV == "production"){
+    //     // app.use(path.join(__dirname, "./../../client/build"));
+    //     // app.get('*', (req, res) => {
+    //     //     res.sendFile(path.join(__dirname, "./../../client/build", "index.html"));
+    //     // });
+    //     app.listen(process.env.PORT, () => {
+    //         console.log(`🚀 Server ready`);
+    //     })
+    // }
+    // else {
+    //     app.listen(3001, () => {
+    //         console.log(`🚀 Server ready at http://localhost:3001/graphql`);
+    //     })
+    // }
+
+    app.listen(process.env.PORT || 3001, () => {
+        console.log(`🚀 Server ready`);
+    })
 
 };
 
